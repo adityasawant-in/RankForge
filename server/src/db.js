8828a449f92ded042ecb75fddce645c818fc9e37
adityasawant-in <adityasawant208@gmail.com>
2026-07-03 17:04:53 +0530

    fix: update db.js SSL domain checks to support pooler.supabase.com

--- server/src/db.js
+++ server/src/db.js
@@ -10,7 +10,7 @@ let pool = null;
 if (process.env.DATABASE_URL) {
   pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL.includes("supabase.co")
+    ssl: process.env.DATABASE_URL.includes("supabase")
       ? { rejectUnauthorized: false }
       : false
   });
